538c31d9d4404175d197bf9120364b0dc2f8df89
Guillaume EGRET <guegret@gmail.com>
2023-01-27 14:24:31 +0100

    changed limits

--- go_kubernetes_deployment/kube_deployment.go
+++ go_kubernetes_deployment/kube_deployment.go
@@ -122,7 +122,7 @@ func createDeployment(url, quality, fps, streamerName string, deploymentsClient 
 							},
 							Resources: apiv1.ResourceRequirements{
 								Limits: apiv1.ResourceList{
-									apiv1.ResourceCPU:    resource.MustParse("1"),
+									apiv1.ResourceCPU:    resource.MustParse("2"),
 									apiv1.ResourceMemory: resource.MustParse("500Mi"),
 								},
 							},
